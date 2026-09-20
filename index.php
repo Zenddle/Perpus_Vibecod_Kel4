c24efd5ebab6678791f8101a186a96f15f4960a5
yunitaprs58 <yunitaprs58@gmail.com>
2026-09-20 16:15:58 +0800

    yunita push

--- index.php
+++ index.php
@@ -38,3 +38,5 @@ spl_autoload_register(function (string $class) {
 $router = new App\Core\Router();
 require APP_PATH . '/Routes/web.php';
 $router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+
+//test kontribusi
